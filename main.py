252db6228893decceb959154d0314b857f75c8bb
Wiktoria Stepien <stepwiktoriaa@gmail.com>
2023-05-23 23:37:14 +0200

    database, notes and everything

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-from flask import Flask, render_template
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/create')
-def create_note():
-    return render_template('create_note.html')
-
-@app.route('/delete')
-def delete_note():
-    return render_template('delete_note.html')
+app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -1,29 +1,37 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def menu():
-    return render_template('menu.html')
 
-@views.route('/home')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template('home.html')
+    if request.method == 'POST': 
+        note = request.form.get('note')#Gets the note from the HTML 
 
-@views.route('/create_note', methods=['GET', 'POST'])
-def create_note():
-    if request.method == 'POST':
-        # Logika tworzenia notatki
-        # Pobierz dane z formularza
-        title = request.form.get('title')
-        content = request.form.get('content')
-        # Wykonaj operacje zapisu notatki (np. w bazie danych)
-        # ...
-        return 'Notatka została utworzona!'
-    return render_template('create_note.html')
+        if len(note) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
+            db.session.add(new_note) #adding the note to the database 
+            db.session.commit()
+            flash('Note added!', category='success')
 
-@views.route('/delete_note')
-def delete_note():
-    # Logika usuwania notatki
-    # ...
-    return 'Notatka została usunięta!'
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
