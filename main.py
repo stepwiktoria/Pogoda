2a2a68a52d7216fad981d4a58c3c8d967d729824
Wiktoria Stepien <stepwiktoriaa@gmail.com>
2023-05-23 22:49:02 +0200

    menu and main

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from website import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/create')
+def create_note():
+    return render_template('create_note.html')
+
+@app.route('/delete')
+def delete_note():
+    return render_template('delete_note.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
