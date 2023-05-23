cf0e7ec245616fb18563831f7b11e896262589a6
Filip <flips.talarek17@gmail.com>
2023-05-24 01:19:05 +0200

    jest

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-#FILIP TU
+#FILIP TU jest
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
